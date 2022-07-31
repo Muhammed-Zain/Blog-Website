281ee5ef4dd09b548b6fd38a258000a58647b4ca
Muhammed Zain <mohdzain2002@gmail.com>
2022-07-31 22:50:52 +0530

    Added the files with gitignore

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+
+const homeStartingContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.";
+
+const aboutContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.";
+
+const contactContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.";
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { StartingContent: homeStartingContent });
+});
+
+app.listen(4000, () => {
+  console.log("The website is running successfully on port 4000");
+});
